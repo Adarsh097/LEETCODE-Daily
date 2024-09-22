6664237075dabf3e36bde55c01e5d55eef80e910
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2024-09-22 09:52:42 +0530

    Time: 255 ms (74.39%), Space: 121.9 MB (81.09%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,35 +1,38 @@
 class MedianFinder {
 private:
-    priority_queue<int> leftMaxHeap;
-    priority_queue<int,vector<int>,greater<int>>rightMinHeap;
+    priority_queue<int>maxLeft;
+    priority_queue<int,vector<int>,greater<int>>minRight;
 public:
     MedianFinder() {
         
     }
     
     void addNum(int num) {
-        if(leftMaxHeap.empty() || num < leftMaxHeap.top()){
-            leftMaxHeap.push(num);
+        if(maxLeft.empty() || num < maxLeft.top()){
+            maxLeft.push(num);
         }else{
-            rightMinHeap.push(num);
+            minRight.push(num);
         }
 
-        //balance the size
-        if(abs((int)leftMaxHeap.size() - (int)rightMinHeap.size())>1){
-            rightMinHeap.push(leftMaxHeap.top());
-            leftMaxHeap.pop();
-        }else if(rightMinHeap.size() > leftMaxHeap.size()){
-            leftMaxHeap.push(rightMinHeap.top());
-            rightMinHeap.pop();
+        //balance
+        // keeping size of leftHeap atmost one greater than the right
+        // but size of right heap shouldn't be greater than the size of the left heap
+        if(abs((int)maxLeft.size() - (int)minRight.size())>1){
+            minRight.push(maxLeft.top());
+            maxLeft.pop();
+        }else if(minRight.size() > maxLeft.size()){
+            maxLeft.push(minRight.top());
+            minRight.pop();
         }
     }
     
     double findMedian() {
-        if(leftMaxHeap.size() == rightMinHeap.size()){
-            double ans = (leftMaxHeap.top() + rightMinHeap.top())/2.0;
-            return ans;
+        if(maxLeft.size() != minRight.size()){
+            return maxLeft.top();
         }else{
-            return leftMaxHeap.top();
+            double ans = (maxLeft.top() + minRight.top())/2.0;
+            return ans
+            ;
         }
     }
 };
