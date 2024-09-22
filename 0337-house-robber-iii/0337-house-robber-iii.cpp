b40dfc7a6744ef589fa47b44875697cb3c60c624
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2024-09-22 14:39:42 +0530

    Time: 20 ms (24.06%), Space: 24.8 MB (48.35%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -13,16 +13,13 @@ class Solution {
 public:
     unordered_map<TreeNode*,int>m;
     int rob(TreeNode* root) {
-        if(m.find(root) !=m.end()){
+        if(root==NULL) return 0;
+
+        if(m.find(root)!=m.end()){
             return m[root];
         }
-        if(root==nullptr) return 0;
 
-        int ll = 0;
-        int lr = 0;
-        int rl = 0;
-        int rr = 0;
-
+        int ll = 0, lr = 0, rl = 0, rr = 0;
         if(root->left){
             ll = rob(root->left->left);
             lr = rob(root->left->right);
@@ -31,15 +28,11 @@ public:
             rl = rob(root->right->left);
             rr = rob(root->right->right);
         }
-        int curr = root->val + ll + lr + rl + rr;
+        int curr = root->val + ll + lr + rr + rl;
 
         int l = rob(root->left);
         int r = rob(root->right);
 
-        int withoutCurr = l + r;
-
-        return m[root] =  max(curr,withoutCurr);
-
-
+        return m[root] =  max(curr,l+r);
     }
 };
