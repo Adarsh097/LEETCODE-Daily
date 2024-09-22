6d903fa86bbbd1a11af57b55a058e84614631575
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2024-09-22 09:06:41 +0530

    Time: 54 ms (41.06%), Space: 48.4 MB (68%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,37 +1,31 @@
-struct Node{ // making the struct node
-     //each node has 26 links for every character and flag -> to mark the end for a particular word
-     Node *links[26];
-     bool flag = false;
+struct Node{
 
-     //to check if the given node contains the reference node of the character passed 
-     bool containsKey(char ch){
-        return (links[ch - 'a'] != NULL);
-     }
+    Node* links[26];
+    bool flag = false;
 
-     // to put the reference of the new node at the given character
-    void put(char ch,Node* node){
-        links[ch-'a'] = node;
+    bool containsKey(char ch){
+        return (links[ch - 'a']!=NULL);
     }
 
-    // to get the reference node of the given character
+    void put(char ch, Node* node){
+        links[ch -  'a'] = node;
+    }
+
     Node* get(char ch){
-        return links[ch-'a'];
+        return links[ch - 'a'];
     }
 
-    // to set the flag of  the current node as true -> to mark the end of the word.
     void setEnd(){
         flag = true;
     }
 
-    // to check if the current node's flag is true -> to check the end of the word.
     bool isEnd(){
         return flag;
     }
+
 };
 class Trie {
-    // initially we need the root;
 private:
-// creating the data member of Node* type
     Node* root;
 public:
     Trie() {
@@ -39,45 +33,35 @@ public:
     }
     
     void insert(string word) {
-        // tc -> O(len)
         Node* node = root;
         for(int i=0;i<word.length();i++){
             if(!node->containsKey(word[i])){
                 node->put(word[i],new Node());
             }
-            // move to the reference trie
-          node =   node -> get(word[i]);
+            node = node->get(word[i]);
         }
-        // after inserting all the character -> set the end
         node->setEnd();
-        
     }
     
     bool search(string word) {
-        // You start with the root node
-        // tc -> O(length of the word)
-        Node* node = root;
+         Node* node = root;
         for(int i=0;i<word.length();i++){
             if(!node->containsKey(word[i])){
                 return false;
             }
-            // go to the reference trie-node
-            node = node -> get(word[i]);
+            node = node->get(word[i]);
         }
-        return node->isEnd(); // to check if marked 'true'
-
+       return node->isEnd();
     }
     
     bool startsWith(string prefix) {
-        // tc -> O(len)
-        Node* node = root;
+          Node* node = root;
         for(int i=0;i<prefix.length();i++){
             if(!node->containsKey(prefix[i])){
                 return false;
             }
-            node = node -> get(prefix[i]);
+            node = node->get(prefix[i]);
         }
-        // traverse whole of the prefix -> it's exits in the trie
         return true;
     }
 };
