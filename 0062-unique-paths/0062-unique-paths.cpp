dc9e461f4f3d6161373ccc7c753beb0c24900614
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2024-09-25 12:34:24 +0530

    Time: 4 ms (12.89%), Space: 8 MB (21.75%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    int solve(int m,int n){
-        if(m==0 && n==0)return 1;
+    int solve(int m, int n){
+        if(m==0 && n==0){
+            // one way to reach here
+            return 1;
+        }
+        if(m<0 || n<0){
+            //out of bound
+            return 0;
+        }
+        if(dp[m][n] != -1)return dp[m][n];
 
-        if(m<0 || n<0)return 0;
+        int opt1 = solve(m-1,n);
+        int opt2 = solve(m,n-1);
 
-        if(dp[m][n]!=-1) return dp[m][n];
-
-        int up = solve(m-1,n);
-        int down = solve(m,n-1);
-
-        return dp[m][n] = up + down;
+        return dp[m][n] = opt1 + opt2;
     }
     int uniquePaths(int m, int n) {
         dp.resize(m,vector<int>(n,-1));
         int ans = solve(m-1,n-1);
+    
         return ans;
     }
 };
