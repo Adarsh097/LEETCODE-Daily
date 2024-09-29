2cbf8fc891fc1aaf6ec75af416195dbc9186e3c1
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2024-09-29 17:31:26 +0530

    Time: 364 ms (62.92%), Space: 203.8 MB (91.01%) - LeetHub

--- 2778-frequency-tracker/2778-frequency-tracker.cpp
+++ 2778-frequency-tracker/2778-frequency-tracker.cpp
@@ -1,48 +1,53 @@
 class FrequencyTracker {
-    unordered_map<int, int> m;         // Tracks the frequency of each number
-    unordered_map<int, int> freqCount; // Tracks how many numbers have a specific frequency
-
+    unordered_map<int,int>m;
+    unordered_map<int,int>freq;
 public:
     FrequencyTracker() {
-        // Constructor
+        
     }
-
+    
     void add(int number) {
-        int currentFreq = m[number];
-        if (currentFreq > 0) {
-            // Reduce the count of the old frequency
-            freqCount[currentFreq]--;
-            if (freqCount[currentFreq] == 0) {
-                freqCount.erase(currentFreq); // Clean up to avoid unnecessary entries
-            }
-        }
+        int currFreq = m[number];
 
-        // Increment the frequency of the number
+        if(currFreq > 0){
+            freq[currFreq]--;
+
+            if(freq[currFreq] == 0){
+                freq.erase(currFreq);
+            }
+
+        }
         m[number]++;
-        freqCount[m[number]]++; // Increase the count of the new frequency
+        freq[m[number]]++;
     }
-
+    
     void deleteOne(int number) {
-        if (m.find(number) != m.end() && m[number] > 0) {
-            int currentFreq = m[number];
-            // Reduce the count of the current frequency
-            freqCount[currentFreq]--;
-            if (freqCount[currentFreq] == 0) {
-                freqCount.erase(currentFreq); // Clean up
+        if(m.find(number)!=m.end() && m[number]>0){
+            int currFreq = m[number];
+
+            freq[currFreq]--;
+            if(freq[currFreq] == 0){
+                freq.erase(currFreq);
             }
 
-            // Decrease the frequency of the number
             m[number]--;
-            if (m[number] == 0) {
-                m.erase(number); // Remove the number if its frequency becomes zero
-            } else {
-                freqCount[m[number]]++; // Update frequency count
+            if(m[number] == 0){
+                m.erase(number);
+            }else{
+                freq[m[number]]++;
             }
         }
     }
-
+    
     bool hasFrequency(int frequency) {
-        // Check if any number has the given frequency in O(1)
-        return freqCount.find(frequency) != freqCount.end();
+        return freq.find(frequency) != freq.end();
     }
 };
+
+/**
+ * Your FrequencyTracker object will be instantiated and called as such:
+ * FrequencyTracker* obj = new FrequencyTracker();
+ * obj->add(number);
+ * obj->deleteOne(number);
+ * bool param_3 = obj->hasFrequency(frequency);
+ */
