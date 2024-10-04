16e4cc3ee92e3e4e64a512ce4560e966313ea183
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2024-10-04 09:44:11 +0530

    Time: 6 ms (62.6%), Space: 17.1 MB (55.42%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-   
-        int startColor = image[sr][sc];
-            if(startColor == color)return image;
-        image[sr][sc] = color;
-        int m = image.size();
-        int n = image[0].size();
-
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
-        int row[4] = {1,0,-1,0};
-        int col[4] = {0,1,0,-1};
-
-        while(!q.empty()){
-            int r = q.front().first;
-            int c = q.front().second;
-            q.pop();
-
-            for(int i=0;i<4;i++){
-                int nr = r + row[i];
-                int nc = c + col[i];
-
-                if(nr>=0 && nr<m && nc>=0 && nc<n &&image[nr][nc]==startColor){
-                    image[nr][nc] = color;
-                    q.push({nr,nc});
-                }
+    //4-directionally travel
+    int row[4] = {1,0,-1,0};
+    int col[4] = {0,1,0,-1};
+     vector<vector<int>>arr;
+     void dfs(int sr,int sc,int color,int initialColor){
+        arr[sr][sc] = color;
+        for(int i=0;i<4;i++){
+            int nr = sr + row[i];
+            int nc = sc + col[i];
+            if(nr>=0 && nr<arr.size() && nc>=0 && nc<arr[0].size() && arr[nr][nc]==initialColor){
+                dfs(nr,nc,color,initialColor);
             }
         }
-        return image;
+        return;
+     }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int initialColor = image[sr][sc];
+        if(initialColor == color)return image;
+        arr = image;
+        dfs(sr,sc,color,initialColor);
+        return arr;
+
     }
 };
