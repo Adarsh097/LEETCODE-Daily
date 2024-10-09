33d8f6c9dc22202736e9787fa0c40e629653e7d7
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2024-10-09 14:39:20 +0530

    Time: 108 ms (47.72%), Space: 32.8 MB (53.29%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -3,14 +3,17 @@ public:
     int minSwaps(string s) {
         int close = 0;
         int open = 0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='[')open++;
-           else{
-            if(open > 0)open--;
-            else close++;
-           }
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '[')
+                open++;
+            else {
+                if (open > 0)
+                    open--;
+                else
+                    close++;
+            }
         }
-       
-        return (open+1)/2;
+
+        return (open + 1) / 2;
     }
 };
