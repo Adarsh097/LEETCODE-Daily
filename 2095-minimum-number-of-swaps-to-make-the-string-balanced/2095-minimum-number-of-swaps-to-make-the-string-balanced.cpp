5e74c05be62736df810ea96deee73160fa7bb6ad
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2024-10-09 14:29:11 +0530

    Time: 121 ms (31.92%), Space: 35.1 MB (44.34%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-// ]][[ -> 1 swap
-// ]]][[[ -> 2 swap
-// ]]]][[[[ -> 2 swap
-// total swaps for this -> ((total opening brackets or total closing brackets)+1)/2 
     int minSwaps(string s) {
-        int n = s.size();
-        stack<int>st;
-        int countSwap = 0;
+        stack<char>st;
+        int n=s.size();
+        int count = 0;
         for(int i=0;i<n;i++){
             char ch = s[i];
-            if(ch == '['){
+            if(ch=='['){
                 st.push(ch);
             }else{
                 if(!st.empty()){
-                    st.pop(); // to pop the complementary bracket
+                    st.pop();
                 }else{
-                    //no complementary bracket to pop from the stack coz of reversing
-                    countSwap++;
+                    count++;
                 }
             }
         }
-        return (countSwap + 1)/2;
+        return (count+1)/2;
     }
 };
