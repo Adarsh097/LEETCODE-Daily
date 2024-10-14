f6a332204a696717b8ca1fe5ee690ed160f9e61f
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2024-10-14 21:53:44 +0530

    Time: 0 ms (100%), Space: 8.4 MB (16.13%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    double solve(double x,long n){
-        if(n==0)return 1.0;
-        if(n<0)return solve(1/x,-n);
-        if((n&1)!=1) return solve(x*x,n/2);
-        else return  x*solve(x*x,(n-1)/2);
-    }
     double myPow(double x, int n) {
-        return solve(x,(long)n);
+        double ans = 1.0;
+        long long m = n;
+        if(n<0){
+            m = -m;
+        }
+        while(m){
+            if(m&1){
+                ans = ans * x;
+                m = m-1;
+            }else{
+                x = x*x;
+                m = m/2;
+            }
+        }
+        if(n<0)return 1.0/(double)ans;
+        return ans;
     }
 };
-// range -> 2^(-31) to 2^(31)-1
-// x^(-n), where n == INT_MIN; => (1/x)^n will result in overflow -> take long for the n
-
-//binary exponentiation ->
-/*
-    x^n => (x*x)^(n/2) ,if n is even
-    x^n => x*((x*x)^(n/2)) ,if n is odd
- */
