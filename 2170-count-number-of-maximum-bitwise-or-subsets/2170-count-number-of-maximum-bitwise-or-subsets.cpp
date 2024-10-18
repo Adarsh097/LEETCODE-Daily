0b7a45e222b56c845f6d16af301e5ec73249e28f
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2024-10-18 08:39:38 +0530

    Time: 6 ms (97.88%), Space: 10 MB (93.24%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    void count(int &ans,vector<int>&nums,int target,int ind, int &size,int bitwise){
-       
+    int count(vector<int>&nums,int target,int ind, int &size,int bitwise){
         if(ind >= size){
-             if(bitwise == target){
-            ans++;
+             if(bitwise == target)return 1;
+             else return 0;
         }
-        return;
-        }
-
+        int ans = 0;
         //take 
-        count(ans,nums,target,ind+1,size,bitwise|nums[ind]);
-
+        ans += count(nums,target,ind+1,size,bitwise|nums[ind]);
         //not take
-        count(ans,nums,target,ind+1,size,bitwise);
-        return;
-
+        ans += count(nums,target,ind+1,size,bitwise);
+        return ans;
     }
     int countMaxOrSubsets(vector<int>& nums) {
+        int n = nums.size();
         int maxBitwise = 0;
         for(auto &val : nums){
             maxBitwise |= val;
         }
-        int n = nums.size();
-        int ans = 0;
-        int bitwise = 0;
-        count(ans,nums,maxBitwise,0,n,bitwise);
+        int ans = count(nums,maxBitwise,0,n,0);
         return ans;
     }
 };
