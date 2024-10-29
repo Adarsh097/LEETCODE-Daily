082e377f2c9a163b85f39cb9aa9784673c6ed02c
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2024-10-29 11:15:48 +0530

    Time: 145 ms (71.73%), Space: 99.7 MB (18.08%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     vector<vector<int>>dp;
-    bool solve(vector<int>&nums,int ind,int &n,int target){
+    bool solveM(vector<int>& nums,int n,int target){
         if(target == 0)return true;
-        if(target < 0 || ind >= n)return false;
-        if(dp[ind][target]!=-1)return dp[ind][target];
-        //take or not take
-        bool notTaken = solve(nums,ind+1,n,target);
+        if(target<0 || n == 0)return false;
+        if(dp[n][target] != -1)return dp[n][target];
+        // take
+        int opt1 = solveM(nums,n-1,target - nums[n-1]);
 
-        bool taken;
-        if(nums[ind]<=target){
-            taken = solve(nums,ind+1,n,target-nums[ind]);
-        }
-        return dp[ind][target] = notTaken || taken;
+        // not take 
+        int opt2 = solveM(nums,n-1,target);
+
+        return dp[n][target] = opt1 || opt2;
     }
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(),nums.end(),0);
-        cout<<sum<<endl;
-        if(sum%2!=0)return false;
-        int target = sum/2;
-        cout<<target<<endl;
         int n = nums.size();
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        if((sum & 1)==1)return false;
+        int target  = sum/2;
         dp.resize(n+1,vector<int>(target+1,-1));
-        bool ans = solve(nums,0,n,target);
+        bool ans = solveM(nums,n,target);
         return ans;
     }
 };
