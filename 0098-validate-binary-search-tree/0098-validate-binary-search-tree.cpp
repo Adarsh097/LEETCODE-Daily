af43c276667291c50f6250e114168d9dbec5fb89
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2024-11-22 10:49:58 +0530

    Time: 0 ms (100%), Space: 20.4 MB (53.42%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,23 +11,17 @@
  */
 class Solution {
 public:
-    /* 
-        1. here, value of the root can be upto -> INT_MIN or INT_MAX
-        2. You should take greater range for the min and max range values.
-     */
-    bool solve(TreeNode* &root, long min, long max){
-        if(root==NULL)return true;
-
-         if(root->val < max && root->val > min){
-            bool leftAns = solve(root->left,min,root->val);
-            bool rightAns = solve(root->right,root->val,max);
-            return leftAns && rightAns;
+    bool solve(TreeNode*&root,long min,long max){
+        if(!root)return true;
+        if(root->val < max && root->val > min){
+            bool left = solve(root->left,min,root->val);
+            bool right  = solve(root->right,root->val,max);
+            return left && right;
         }
         return false;
     }
     bool isValidBST(TreeNode* root) {
-        if(root==NULL || (root->left==NULL && root->right==NULL))return true;
-      bool ans = solve(root,LONG_MIN,LONG_MAX);
-      return ans;
+        bool ans = solve(root,LONG_MIN,LONG_MAX);
+        return ans;
     }
 };
