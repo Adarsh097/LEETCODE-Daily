ffad66031831a27ed89566dfb1f3533617f35241
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2025-01-25 00:45:08 +0530

    Time: 4 ms (43.52%), Space: 8.9 MB (98.91%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s == t;
+        vector<int>arr(26,0);
+        if(s.size() != t.size())return false;
+        for(int i=0;i<s.size();i++){
+            arr[s[i]-'a']++;
+            arr[t[i]-'a']--;
+        }
+        for(auto &val : arr){
+            if(val !=0 )return false;
+        }
+        return true;
     }
 };
