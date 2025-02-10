5ac307b0e741b6038b2a4d1d05f68bb042eed947
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2025-02-10 09:32:24 +0530

    Time: 0 ms (100%), Space: 8.5 MB (56.73%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -3,20 +3,15 @@ public:
     string clearDigits(string s) {
         int cnt = 0;
        
-        
+        string ans = "";
         for(int i=s.size()-1;i>=0;i--){
             if(s[i]>='0' && s[i]<='9')cnt++;
             else if(s[i]>='a' && s[i]<='z'){
                 if(cnt>0){
-                    s[i] = ' ';
                     cnt--;
+                }else{
+                    ans = s[i] + ans;
                 }
-            }
-        }
-        string ans = "";
-        for(auto &ch : s){
-            if(ch>='a'&& ch<='z'){
-                ans += ch;
             }
         }
         return ans;
