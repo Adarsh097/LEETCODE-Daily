1bce0ceb768c35a5bb2168016f499db167fb3277
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2025-02-12 20:56:36 +0530

    Time: 0 ms (100%), Space: 8.2 MB (12.68%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        string str = to_string(num);
-        for(int i=0;i<str.size();i++){
-            if(str[i]=='6'){
-                str[i] = '9';
-                break;
+        int pos = -1;
+        int temp = 0;
+        int n = num;
+        while(n>0){
+            int digit = n%10;
+            n/=10;
+            if(digit==6){
+                pos = temp;
             }
+            temp++;
         }
-        int ans = stoi(str);
-        return ans;
+        int toAdd = 3*pow(10,pos);
+        return (pos>=0)?toAdd+num:num;
     }
 };
