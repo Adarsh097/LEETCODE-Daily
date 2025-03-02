1b826e78dda9f358647caab4027b6907fd6bf42d
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2025-03-02 10:19:12 +0530

    Time: 1748 ms (17.96%), Space: 9.5 MB (60.25%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
-    long long dp[2001];
-    bool isPalin(string &s,int i,int j){
-        
+    int n;
+    vector<int>dp;
+    bool isPalin(string &s, int i,int j){
         while(i<j){
             if(s[i]!=s[j])return false;
-            i++;
-            j--;
+            else{
+                i++;j--;
+            }
         }
         return true;
     }
-    long long solve(string &s,int &n,int i){
-        if(i==n)return 0;
+    int solve(string &s,int ind){
+        if(ind>=n)return 0;
+        if(dp[ind]!=-1)return dp[ind];
 
-        if(dp[i]!=-1)return dp[i];
-
-        long long minCuts = INT_MAX;
-        
-        for(int k=i;k<n;k++){
-            
-            if(isPalin(s,i,k)){
-                long long cuts = 1 + solve(s,n,k+1);
-                minCuts = min(minCuts,cuts);
+        int mini = INT_MAX;
+        for(int k=ind;k<n;k++){
+            if(isPalin(s,ind,k)){
+                int temp = 1 + solve(s,k+1);
+                mini  = min(mini,temp);
             }
         }
-        return dp[i] = minCuts;
+        return dp[ind] = mini;
     }
     int minCut(string s) {
-        memset(dp,-1,sizeof(dp));
-        int n = s.size();
-        long long cuts = solve(s,n,0);
-        return (int)cuts-1;
+        n = s.size();
+        dp.resize(n+1,-1);
+        int ans = solve(s,0);
+        return ans-1;
     }
 };
