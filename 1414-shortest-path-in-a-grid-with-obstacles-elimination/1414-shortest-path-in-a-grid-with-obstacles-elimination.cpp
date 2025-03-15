2de162a3e1fe48ccd2f7ba266d6a8355d919efc3
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2025-03-15 13:28:29 +0530

    Time: 1353 ms (5.01%), Space: 393.3 MB (5.01%) - LeetHub

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/1414-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,43 +1,37 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <tuple>
-
-using namespace std;
-
 class Solution {
 public:
-    int row[4] = {1, 0, -1, 0};
-    int col[4] = {0, 1, 0, -1};
-
+    int row[4] = {1,0,-1,0};
+    int col[4] = {0,1,0,-1};
     int shortestPath(vector<vector<int>>& grid, int k) {
-        int m = grid.size(), n = grid[0].size();
-        if (grid[0][0] == 1 && k == 0) return -1;
+        int m  = grid.size(),
+        n = grid[0].size();
+        if(grid[0][0]==1 && k==0)return -1;
 
-        queue<tuple<int, int, int, int>> q;
-        vector<vector<vector<bool>>> vis(m, vector<vector<bool>>(n, vector<bool>(k + 1, false)));
-
-        if (grid[0][0] == 1) k--;
+        queue<pair<int,vector<int>>>q;
+       vector<vector<vector<bool>>> vis(m, vector<vector<bool>>(n, vector<bool>(k + 1, false)));
+        if(grid[0][0]==1)k--;
         vis[0][0][k] = true;
-        q.push({0, 0, 0, k});
+        q.push({0,{0,0,k}});
         int ans = INT_MAX;
-        while (!q.empty()) {
-            auto [dist, r, c, remK] = q.front();
+        while(!q.empty()){
+            auto it = q.front();
             q.pop();
-
-            if (r == m - 1 && c == n - 1 && dist<ans) ans =  dist; 
-
-            for (int i = 0; i < 4; i++) {
+            int dist = it.first,
+            r = it.second[0], c = it.second[1],
+            remK = it.second[2];
+            if(r==m-1 && c==n-1 && dist < ans)ans = dist;
+            for(int i=0;i<4;i++){
                 int x = r + row[i], y = c + col[i];
-                if (x >= 0 && y >= 0 && x < m && y < n) {
+                if(x>=0 && y>=0 && x<m && y<n){
                     int newK = remK - grid[x][y];
-                    if (newK >= 0 && !vis[x][y][newK]) {
+                    if(newK>=0 && !vis[x][y][newK]){
                         vis[x][y][newK] = true;
-                        q.push({dist + 1, x, y, newK});
+                        q.push({dist+1,{x,y,newK}});
                     }
                 }
             }
+
         }
-        return (ans==INT_MAX)?-1:ans;
+        return (ans==INT_MAX)?-1 : ans;
     }
 };
