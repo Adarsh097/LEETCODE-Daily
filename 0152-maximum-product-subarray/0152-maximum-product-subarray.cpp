b8fc6abf5fdcf113cf1b1095b78bd73f26a68a08
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2025-03-27 23:16:22 +0530

    Time: 417 ms (5.14%), Space: 17.8 MB (57.43%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        // all positves -> consider  all
-        // even negatives -> consider all
-        // odd negatives -> leave one negative and consider all
-        // contains zero
-      
-        int product = INT_MIN;
-        int prev = 1;
-        int suff = 1;
+        int ans = INT_MIN;
         int n = nums.size();
         for(int i=0;i<n;i++){
-            prev = prev * nums[i];
-            suff = suff * nums[n-1-i];
-            product = max({product,prev,suff});
-            if(prev == 0)prev = 1;
-            if(suff == 0)suff = 1;
-        } 
-        return product;
+            int temp = 1;
+            for(int j=i;j<n;j++){
+                temp*=nums[j];
+                ans = max(ans,temp);
+            }
+            
+        }
+        return ans;
     }
 };
