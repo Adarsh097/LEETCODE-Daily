0cb2566f542c47d68f3436ec60decd25171d9e68
Adarsh Gupta <120124805+Adarsh097@users.noreply.github.com>
2025-05-06 10:01:46 +0530

    Add solution for Find the Duplicate Number

--- LeetCode/Find-the-Duplicate-Number/Solution.cpp
+++ LeetCode/Find-the-Duplicate-Number/Solution.cpp
@@ -1,29 +1,17 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        // mark the indices as negative equal to number to find the ans -> T: O(n) and S: O(1) -> but modifying the array
-        
-        // slow and fast pointer -> T: O(n) and S:O(1) -> optimised algorithm
-
-        int slow = nums[0];
-        int fast = nums[0];
-
-        // find the common point
-        do{
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        }while(slow != fast);
-
-
-        // find the point where the cycle starts
-        slow  = nums[0];
-        while(slow != fast){
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-        return slow;
-
-
+       int slow = nums[0], fast = nums[0];
+       do{
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+       }while(slow!=fast);
+      slow = nums[0];
+      while(slow!=fast){
+        slow = nums[slow];
+        fast = nums[fast];
+       }
+       return slow;
 
     }
 };
