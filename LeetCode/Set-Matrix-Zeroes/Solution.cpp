80cf3257e36672c2fb9b00a1bf16b48aaae95557
Adarsh Gupta (Back-end Intern at GoComet) <120124805+Adarsh097@users.noreply.github.com>
2025-05-21 09:30:19 +0530

    Add solution for Set Matrix Zeroes

--- LeetCode/Set-Matrix-Zeroes/Solution.cpp
+++ LeetCode/Set-Matrix-Zeroes/Solution.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        int zeroCol = 0;
+        
         int m = matrix.size(), n = matrix[0].size();
-        vector<int> col(n, -1);
-        vector<int> row(m, -1);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    row[i] = 0;
-                    col[j] = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0] = 0;
+
+                    if(j==0){
+                        zeroCol = 1;
+                    }else{
+                        matrix[0][j] = 0;
+                    }
                 }
             }
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (row[i]==0 || col[j]==0) {
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j] = 0;
                 }
             }
         }
-        return;
+
+        if(matrix[0][0]==0){
+            for(int i=0;i<n;i++){
+                matrix[0][i] = 0;
+            }
+        }
+        if(zeroCol){
+            for(int i=0;i<m;i++){
+                matrix[i][0] = 0;
+            }
+        }
+        return ;
     }
 };
