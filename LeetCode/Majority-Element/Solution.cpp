e0968c98e0730298e8f0da9f1b530cb8d5e9fef4
Adarsh Gupta (Back-end Intern at GoComet) <120124805+Adarsh097@users.noreply.github.com>
2025-06-11 23:55:52 +0530

    Add solution for Majority Element

--- LeetCode/Majority-Element/Solution.cpp
+++ LeetCode/Majority-Element/Solution.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int ele = 0, cnt = 0;
+        int cnt = 0;
+        int ele = 0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==ele){
-                cnt++;
-            }else{
-               if(cnt==0){
+           if(nums[i]==ele)cnt++;
+           else{
+            if(cnt==0){
                 ele = nums[i];
                 cnt = 1;
-               }else{
+            }else{
                 cnt--;
-               }
             }
+           }
         }
         return ele;
     }
