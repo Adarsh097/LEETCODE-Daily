3d498b489db57c39c4fe90f5976799a197dea09a
Adarsh Gupta (Back-end Intern at GoComet) <120124805+Adarsh097@users.noreply.github.com>
2025-06-15 17:02:43 +0530

    Add solution for Merge Two Sorted Lists

--- LeetCode/Merge-Two-Sorted-Lists/Solution.cpp
+++ LeetCode/Merge-Two-Sorted-Lists/Solution.cpp
@@ -11,15 +11,29 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(!list1)return list2;
-        if(!list2)return list1;
-
-        if(list1->val < list2->val){
-           list1->next =  mergeTwoLists(list1->next,list2);
-           return list1;
-        }else{
-           list2->next = mergeTwoLists(list1,list2->next);
-           return list2;
+        ListNode* temp1 = list1;
+        ListNode* temp2 = list2;
+        ListNode* ans = new ListNode();
+        ListNode* temp = ans;
+        while(temp1 && temp2){
+            if(temp1->val < temp2->val){
+                temp->next = temp1;
+                temp1 = temp1->next;
+                temp = temp->next;
+                temp->next = NULL;
+            }else{
+                temp->next = temp2;
+                temp2 = temp2->next;
+                temp = temp->next;
+                temp->next = NULL;
+            }
         }
+        if(temp1){
+            temp->next = temp1;
+        }
+        if(temp2){
+            temp->next = temp2;
+        }
+        return ans->next;
     }
 };
