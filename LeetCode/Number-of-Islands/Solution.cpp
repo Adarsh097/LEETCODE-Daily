099856554b40f581e30e61374ed07f05cbcaf307
Adarsh Gupta (Back-end Intern at GoComet) <120124805+Adarsh097@users.noreply.github.com>
2025-06-24 21:27:31 +0530

    Add solution for Number of Islands

--- LeetCode/Number-of-Islands/Solution.cpp
+++ LeetCode/Number-of-Islands/Solution.cpp
@@ -2,26 +2,25 @@ class Solution {
 public:
     int row[4] = {1,0,-1,0};
     int col[4] = {0,1,0,-1};
-    
-    void count(vector<vector<char>>& grid,int i,int j){
+    void dfs(vector<vector<char>>& grid,int i,int j){
         grid[i][j] = '0';
         for(int k=0;k<4;k++){
-            int r = i + row[k];
-            int c = j + col[k];
-            if(r<0 || c<0 || r>=grid.size() || c>=grid[0].size() || grid[r][c]=='0')continue;
-            
-                count(grid,r,c);
-            
+            int r = i + row[k], c = j + col[k];
+            if(r>=0 && c>=0 && r<grid.size() && c<grid[0].size() &&  grid[r][c] == '1'){
+                dfs(grid,r,c);
+            }
         }
         return;
     }
     int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
         int cnt = 0;
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]=='1'){
+                    dfs(grid,i,j);
                     cnt++;
-                    count(grid,i,j);
                 }
             }
         }
