ab4e03552079fc0303c89ef97dbef360ff425fbe
Adarsh Gupta (Ex. Back-end Intern at GoComet) <120124805+Adarsh097@users.noreply.github.com>
2025-07-10 20:36:24 +0530

    Add solution for Word Break

---
+++ LeetCode/Word-Break/Solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    unordered_map<string,bool>m;
+    unordered_map<int,bool>dp;
+    bool solve(string &s,int ind){
+        if(ind==s.size())return true;
+        if(dp.find(ind) != dp.end())return dp[ind];
+        string str = "";
+        for(int i=ind;i<s.size();i++){
+            str += s[i];
+            if(m.find(str)!=m.end()){
+                if(solve(s,i+1))return dp[ind] = true;
+            }
+        }
+        return dp[ind] = false;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        for(auto &word : wordDict){
+            m[word] = true;
+        }
+        bool ans = solve(s,0);
+        return ans;
+    }
+};
