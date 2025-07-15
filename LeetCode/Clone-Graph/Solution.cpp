5f580d763517bf57543d87deeb11ad64f8266315
Adarsh Gupta (Ex. Back-end Intern at GoComet) <120124805+Adarsh097@users.noreply.github.com>
2025-07-15 19:29:57 +0530

    Add solution for Clone Graph

--- LeetCode/Clone-Graph/Solution.cpp
+++ LeetCode/Clone-Graph/Solution.cpp
@@ -21,25 +21,23 @@ public:
 
 class Solution {
 public:
+    unordered_map<Node*,Node*>m;
+    void solve(Node* node){
+        Node* newNode  = new Node(node->val);
+        m[node] = newNode;
+        for(auto &neigh : node->neighbors){
+            if(m.find(neigh)==m.end()){
+                 solve(neigh);
+            }
+            newNode->neighbors.push_back(m[neigh]);
+
+        }
+        return;
+    }
+    
     Node* cloneGraph(Node* node) {
         if(!node)return NULL;
-        // {node,copy}
-        unordered_map<Node*,Node*>m;
-        Node* copy = new Node(node->val,{});
-        m[node] = copy;
-        queue<Node*>q;
-        q.push(node);
-        while(!q.empty()){
-            Node* node = q.front();
-            q.pop();
-            for(Node* neigh : node->neighbors){
-                if(m.find(neigh) == m.end()){
-                    m[neigh] = new Node(neigh->val,{});
-                    q.push(neigh);
-                }
-                m[node]->neighbors.push_back(m[neigh]);
-            }
-        }
-        return copy;
+        solve(node);
+        return m[node];
     }
 };
