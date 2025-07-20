bf9cda8976779a7776d4023b1d093af9dec92bd9
Adarsh Gupta (Ex. Back-end Intern at GoComet) <120124805+Adarsh097@users.noreply.github.com>
2025-07-20 18:07:37 +0530

    Add solution for House Robber

--- LeetCode/House-Robber/Solution.cpp
+++ LeetCode/House-Robber/Solution.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
-    vector<int>dp;
-    int solve(vector<int>&nums,int n){
-        if(n<=0)return 0;
-        if(dp[n] != -1)return dp[n];
+    int solveR(vector<int>&nums,int n){
+        if(n==0)return 0;
+        if(n==1)return nums[0];
 
-        int opt1 = nums[n-1] + solve(nums,n-2);
-        int opt2 = solve(nums,n-1);
-        return dp[n] = max(opt1,opt2);
+        int opt1 = nums[n-1] + solveR(nums,n-2);
+        int opt2 = 0 + solveR(nums,n-1);
+
+        int ans = max(opt1,opt2);
+        return ans;
+
+    }
+    int solveM(vector<int>&nums,int n,vector<int>&dp){
+        if(n==0)return 0;
+        if(n==1)return nums[0];
+        if(dp[n]!=-1)return dp[n];
+        int opt1 = nums[n-1] + solveM(nums,n-2,dp);
+        int opt2 = 0 + solveM(nums,n-1,dp);
+
+        dp[n] = max(opt1,opt2);
+        return dp[n];
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        dp.resize(n+1,-1);
-        int ans = solve(nums,n);
+        // int ans = solveR(nums,n);
+
+        vector<int>dp(n+1,-1);
+        int ans = solveM(nums,n,dp);
         return ans;
     }
 };
