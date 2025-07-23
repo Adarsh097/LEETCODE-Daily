b0aa171608cebfe7b594f5f666ec1d04bd896ef9
Adarsh Gupta (Ex. Back-end Intern at GoComet) <120124805+Adarsh097@users.noreply.github.com>
2025-07-24 00:02:50 +0530

    Add solution for Distance Between Bus Stops

--- LeetCode/Distance-Between-Bus-Stops/Solution.cpp
+++ LeetCode/Distance-Between-Bus-Stops/Solution.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
         vector<int>arr(distance.begin(),distance.end());
+        int sum = accumulate(distance.begin(),distance.end(),0);
        
         int n = distance.size();
         int ind1 = start;
-        int ind2 = destination;
         int dist1 = 0, dist2 = 0;
         
         while(ind1 !=destination){
@@ -13,15 +13,9 @@ public:
             ind1++;
             ind1%=n;
         }
+        return min(dist1,sum-dist1);
       
-        cout<<dist1<<endl;
-        while(ind2 !=start){
-            dist2 += distance[ind2];
-            ind2++;
-            ind2 %=n;
-        }
-        cout<<dist2<<endl;
-        return min(dist1,dist2);
+       
 
         
     }
