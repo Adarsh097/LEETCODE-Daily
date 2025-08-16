754d4add7f99812c741dc1855b62b595b09db5a0
Adarsh Gupta (Ex. Back-end Intern at GoComet) <120124805+Adarsh097@users.noreply.github.com>
2025-08-16 11:07:08 +0530

    Add solution for Maximum 69 Number

--- LeetCode/Maximum-69-Number/Solution.cpp
+++ LeetCode/Maximum-69-Number/Solution.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        int pos = -1;
-        int temp = 0;
-        int n = num;
-        while(n>0){
-            int digit = n%10;
-            n/=10;
-            if(digit==6){
-                pos = temp;
+        string str = to_string(num);
+        for(int i=0;i<str.size();i++){
+            if(str[i]=='6'){
+                str[i] = '9';
+                break;
             }
-            temp++;
         }
-        int toAdd = 3*pow(10,pos);
-        return (pos>=0)?toAdd+num:num;
+        int ans = stoi(str);
+        return ans;
     }
 };
