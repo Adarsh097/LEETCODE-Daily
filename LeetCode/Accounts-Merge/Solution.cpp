3c690123050c8af191d7d9fd7248a27c7da6119e
Adarsh Gupta (Ex. Back-end Intern at GoComet) <120124805+Adarsh097@users.noreply.github.com>
2025-08-27 12:05:08 +0530

    Add solution for Accounts Merge

--- LeetCode/Accounts-Merge/Solution.cpp
+++ LeetCode/Accounts-Merge/Solution.cpp
@@ -1,30 +1,29 @@
-class DisjointSet {
-public:
-    vector<int> parent;
-    vector<int> size;
+class Disjoint{
+    public:
+    vector<int>size,parent;
 
-    DisjointSet(int n) {
-        parent.resize(n + 1);
-        size.assign(n + 1, 1);
-        for (int i = 0; i <= n; i++) {
+    Disjoint(int n){
+        size.resize(n+1,1);
+        parent.resize(n+1);
+        for(int i=0;i<=n;i++){
             parent[i] = i;
         }
     }
-
-    int findUP(int node) {
-        if (parent[node] == node) return node;
-        return parent[node] = findUP(parent[node]);
+    int find(int node){
+        if(node == parent[node])return node;
+        return parent[node] =  find(parent[node]);
     }
 
-    void unionBySize(int u, int v) {
-        int up = findUP(u), vp = findUP(v);
-        if (up == vp) return;
-        if (size[up] > size[vp]) {
-            parent[vp] = up;
-            size[up] += size[vp];
-        } else {
+    void merge(int u,int v){
+        int up = find(u), vp = find(v);
+        if(up == vp)return;
+
+        if(size[up] < size[vp]){
             parent[up] = vp;
             size[vp] += size[up];
+        }else{
+            parent[vp] = up;
+            size[up] += size[vp];
         }
     }
 };
@@ -33,37 +32,38 @@ public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
         int n = accounts.size();
         unordered_map<string,int>m;
-        DisjointSet d(n);
+        Disjoint ds(n);
         for(int i=0;i<n;i++){
+
             for(int j=1;j<accounts[i].size();j++){
                 string em = accounts[i][j];
                 if(m.find(em)==m.end()){
                     m[em] = i;
                 }else{
-                    d.unionBySize(i,m[em]);
+                    ds.merge(i,m[em]);
                 }
             }
         }
 
-        vector<string> list[n];
-        for(const auto &it : m){
-            string em = it.first; int node = it.second;
-            int upNode = d.findUP(node);
-            list[upNode].push_back(em);
+        unordered_map<int,vector<string>>list;
+        for(auto &it: m){
+            string em = it.first; int p = it.second;
+            int up = ds.find(p);
+            list[up].push_back(em);
         }
 
         vector<vector<string>>ans;
         for(int i=0;i<n;i++){
-            if(list[i].size() == 0)continue;
-
+            if(list[i].size()==0)continue;
+            vector<string>t;
+            t.push_back(accounts[i][0]);
             sort(list[i].begin(),list[i].end());
-            vector<string>temp;
-            temp.push_back(accounts[i][0]);
-            for(const auto &em : list[i]){
-                temp.push_back(em);
+            for(auto &em: list[i]){
+                t.push_back(em);
             }
-            ans.push_back(temp);
+            ans.push_back(t);
         }
+
         return ans;
     }
 };
