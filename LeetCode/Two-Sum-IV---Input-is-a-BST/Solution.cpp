86f0b0a3fa6b79e2f9781baaeec48478892df4aa
Adarsh Gupta (Ex. Back-end Intern at GoComet) <120124805+Adarsh097@users.noreply.github.com>
2025-08-27 22:16:35 +0530

    Add solution for Two Sum IV - Input is a BST

---
+++ LeetCode/Two-Sum-IV---Input-is-a-BST/Solution.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_set<int>s;
+    bool findTarget(TreeNode* root, int k) {
+        if(!root)return false;
+        bool leftAns = findTarget(root->left,k);
+        bool rightAns = findTarget(root->right,k);
+
+        int target = k - root->val;
+        bool curr = s.find(target)!=s.end();
+        s.insert(root->val);
+        return leftAns || rightAns || curr;
+
+    }
+};
