3ad614ad85ac34e89881ed27239a7a6fe98c8bba
Adarsh Gupta (Ex. Back-end Intern at GoComet) <120124805+Adarsh097@users.noreply.github.com>
2025-09-27 09:07:54 +0530

    Add solution for Rotting Oranges

--- LeetCode/Rotting-Oranges/Solution.cpp
+++ LeetCode/Rotting-Oranges/Solution.cpp
@@ -1,44 +1,34 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size(), days = 0, cnt = 0, total = 0;
-        queue<pair<int,int>>q;//{i,j}
-
-        int row[4] = {1,0,-1,0};
-        int col[4] = {0,1,0,-1};
-
+        int m = grid.size(), n = grid[0].size();
+        queue<pair<int,int>>q;
+        int days = 0, total = 0, cnt = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j] != 0)total++;
-                if(grid[i][j] == 2)q.push({i,j});
+                if(grid[i][j]!=0){
+                    total++;
+                    if(grid[i][j]==2)q.push({i,j});
+                }
             }
         }
-
+        int r[4] = {1,0,-1,0};
+        int c[4] = {0,1,0,-1};
         while(!q.empty()){
             int size = q.size();
-
-            while(size--){// one level
-                int x = q.front().first, y = q.front().second;
+            for(int i=0;i<size;i++){
+                auto it = q.front();
                 q.pop();
                 cnt++;
-
-                for(int i=0;i<4;i++){
-                    int r = x + row[i];
-                    int c = y + col[i];
-
-                    if(r<0 || c<0 || r>=m || c>=n || grid[r][c]!=1)continue;
-
-                    grid[r][c] = 2;
-                    q.push({r,c});
+                for(int k=0;k<4;k++){
+                    int newRow = it.first + r[k], newCol = it.second + c[k];
+                    if(newRow<0 || newCol <0 || newRow>=m || newCol >=n || grid[newRow][newCol]!=1)continue;
+                    grid[newRow][newCol] = 2;
+                    q.push({newRow,newCol});
                 }
-
             }
-
             if(!q.empty())days++;
-
         }
-
-        return total==cnt?days:-1;
-        
+        return (total==cnt)?days:-1;
     }
 };
